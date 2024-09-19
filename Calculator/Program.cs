3329b5caecfb7e90a0c9393b8827c097c3f69d1f
miraxxel <forremains@yandex.ru>
2024-09-19 21:16:02 +0300

    Calculator update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define CALCULATOR_1
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,28 +11,44 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите выражение: ");
-            string expression = Console.ReadLine();
-            Console.Write(expression);
+#if CALCULATOR_1
+            try 
+            {
+                Console.Write("Введите выражение: ");
+                string expression = Console.ReadLine();
 
-            String[] tokens = expression.Split('+', '-', '*', '/'); // сохр. в массив всё кроме указанного
+                String[] tokens = expression.Split('+', '-', '*', '/', ' '); // сохр. в массив всё кроме указанного
+
+                double a = Convert.ToDouble(tokens[0]);
+                double b = Convert.ToDouble(tokens[1]);
+
+                /*//   Метод Contains() класса 'string' определяет, содержит ли строка указанный символ или подстроку
+                if (expression.Contains('+')) Console.WriteLine(a + b);
+                else if (expression.Contains('-')) Console.WriteLine(a - b);
+                else if (expression.Contains('*')) Console.WriteLine(a * b);
+                else if (expression.Contains('/')) Console.WriteLine(a / b);
+                else Console.WriteLine(("No operation")); 
+                // переделать ^ через свитч кейс*/
+
+                //  Метод IndexOfAny ищет первый индекс в строке expression, где встречается любой из символов, указанных в массиве char
+                char operations = expression[expression.IndexOfAny(new char[] { '+', '-', '*', '/' })];
+                switch (operations)
+                {
+                    case '+': Console.WriteLine($"{a} + {b} = {a + b}"); break;
+                    case '-': Console.WriteLine($"{a} - {b} = {a - b}"); break;
+                    case '*': Console.WriteLine($"{a} * {b} = {a * b}"); break;
+                    case '/': Console.WriteLine($"{a} / {b} = {a / b}"); break;
+                }
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Вероятно, неправильный ввод, убедитесь, что ввод в формате: число оператор число");
+            }
             
-            //for (int i = 0; i < tokens.Length; i++)
-            //{
-            //    Console.Write(tokens[i] + "\t");
-            //}
-            //Console.WriteLine();
+#endif
 
-            double a = Convert.ToDouble(tokens[0]);
-            double b = Convert.ToDouble(tokens[1]);
 
-            //   Метод Contains() класса 'string' определяет, содержит ли строка указанный символ или подстроку
-            if (expression.Contains('+')) Console.WriteLine(a + b);
-            else if (expression.Contains('-')) Console.WriteLine(a - b);
-            else if (expression.Contains('*')) Console.WriteLine(a * b);
-            else if (expression.Contains('/')) Console.WriteLine(a / b);
-            else Console.WriteLine(("No operation")); 
-            // переделать ^ через свитч кейс
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,20 +31,24 @@ namespace Game
                 //Console.WriteLine(key.ToString());
                 switch (key)
                 {
+                    case ConsoleKey.UpArrow:
                     case ConsoleKey.W: y--; break;
+                    case ConsoleKey.DownArrow:
                     case ConsoleKey.S: y++; break;
+                    case ConsoleKey.LeftArrow:
                     case ConsoleKey.A: x -= 2; break;
+                    case ConsoleKey.RightArrow:
                     case ConsoleKey.D: x += 2; break;
                     
                 }
                 if (y < 1) y = 1;
                 if (x < 0) x = 0;
-                if (y > Console.BufferHeight - 1) y = Console.BufferHeight - 2;
-                if (x > Console.BufferWidth - 1) x = Console.BufferWidth - 2;
-
+                if (y > Console.BufferHeight - 1) y = Console.BufferHeight - 1;
+                if (x > Console.BufferWidth - 1) x = Console.BufferWidth - 1;
 
                 Console.Clear();
-                Console.SetCursorPosition(y, x);
+                Console.WriteLine($"X = {x}, Y = {y}");
+                Console.SetCursorPosition(x, y);
                 Console.Write(symbol);
             } while (key != ConsoleKey.Escape);
         }
